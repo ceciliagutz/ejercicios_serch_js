593f6122597879ca819f152be2ecbbacb79552a7
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-23 22:41:25 -0600

    feat: Se agrego el ejercicio 4

--- ejercicios.js
+++ ejercicios.js
@@ -50,4 +50,17 @@ function sumAndProduct(numeros) {
 }
 sumAndProduct([1, 2, 3, 4]);
 
+// --------------EJERCICIO 4------------
+/* 
+Exercise #4 (sugerencia filter() e includes() )
+let student1Courses = ['Math', 'English', 'Programming'];
+let student2Courses = ['Geography', 'Spanish', 'Programming'];
+Create a program that loops over the 2 arrays; if there are any common courses print them out to the console.
+ */
+
+let student1Courses = ['Math', 'English', 'Programming'];
+let student2Courses = ['Geography', 'Spanish', 'Programming'];
+
+let commonCourses = student1Courses.filter(course => student2Courses.includes(course));
+console.log("Common course(s):", commonCourses);
 
