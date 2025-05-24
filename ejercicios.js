6fc2486aff0e40127789a4ed0b92e9c6c5027e89
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-23 22:37:08 -0600

    feat: Se agrego el ejercicio 3

--- ejercicios.js
+++ ejercicios.js
@@ -35,4 +35,19 @@ function elDobleNumeros(numbers) {
 }
 elDobleNumeros([1, 2, 4, 5]);
 
+//---------EJERCICIO 3-----------------
+/* 
+Exercise #3 (sugerencia reduce() )
+Write a program to compute the sum and product (multiplication) of an array of numbers. Print out the sum and the product.
+Example: Given an array [1, 2, 3, 4] The sum is 10. The product is 24.
+
+*/
+function sumAndProduct(numeros) {
+  const suma = numeros.reduce((acc, num) => acc + num, 0); //acc es un acumulador
+  const multiplicacion = numeros.reduce((acc, num) => acc * num, 1);
+  console.log("Suma:", suma);
+  console.log("Multiplicación-Producto:", multiplicacion);
+}
+sumAndProduct([1, 2, 3, 4]);
+
 
