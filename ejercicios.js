fbab95313226ea6ea86676bf751bc21f9f8b79a3
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-23 22:48:45 -0600

    feat: Se agrego el ejercicio 5

--- ejercicios.js
+++ ejercicios.js
@@ -12,7 +12,7 @@ Complete the function to print out the string: This is a sentence.
 */
 let arr = ["This", "is", "a", "sentence."];
 function printOutString() {
-  console.log(arr.join(" "));
+    console.log(arr.join(" "));
 }
 printOutString(); // Imprime: "This is a sentence."
 
@@ -30,10 +30,10 @@ Example: Given an array [1, 2, 4, 5]. The output should be [2, 4, 8, 10]
 
 */
 function elDobleNumeros(numbers) {
-  const elDoble = numbers.map(num => num * 2);
-  console.log(elDoble);
+    const elDoble = numbers.map(num => num * 2);
+    console.log(elDoble);
 }
-elDobleNumeros([1, 2, 4, 5]);
+elDobleNumeros([1, 2, 4, 5]);// el resultado sera 2,4,8,20
 
 //---------EJERCICIO 3-----------------
 /* 
@@ -43,10 +43,10 @@ Example: Given an array [1, 2, 3, 4] The sum is 10. The product is 24.
 
 */
 function sumAndProduct(numeros) {
-  const suma = numeros.reduce((acc, num) => acc + num, 0); //acc es un acumulador
-  const multiplicacion = numeros.reduce((acc, num) => acc * num, 1);
-  console.log("Suma:", suma);
-  console.log("Multiplicación-Producto:", multiplicacion);
+    const suma = numeros.reduce((acc, num) => acc + num, 0); //acc es un acumulador
+    const multiplicacion = numeros.reduce((acc, num) => acc * num, 1);
+    console.log("Suma:", suma);// el producto sera 10
+    console.log("Multiplicación-Producto:", multiplicacion); // el producto sera 24
 }
 sumAndProduct([1, 2, 3, 4]);
 
@@ -62,5 +62,52 @@ let student1Courses = ['Math', 'English', 'Programming'];
 let student2Courses = ['Geography', 'Spanish', 'Programming'];
 
 let commonCourses = student1Courses.filter(course => student2Courses.includes(course));
-console.log("Common course(s):", commonCourses);
+console.log("Common course(s):", commonCourses); // Imprimira "Programming" porque es el curso que se repite
+
+
+//----------EJERCICIO 5 --------------
+/* 
+Exercise #5
+For each of the exercises below, assume you are starting with the following people array.
+let people = ["Maria", "Dani", "Luis", "Juan", "Camila"];
+1.	Write a command that prints out all of the people in the list.
+2.	Write the command to remove "Dani" from the array.
+3.	Write the command to remove "Juan" from the array.
+4.	Write the command to move "Luis" to the front of the array.
+5.	Write the command to add your name to the end of the array.
+6.	Using a loop, iterate through this array and after console.log "Maria", exit from the loop. (usar break)
+7.	Write the command that gives the indexOf where "Maria" is located.
+At the end of the exercise, there should be 4 people in the array.
+
+*/
+let people = ["Maria", "Dani", "Luis", "Juan", "Camila"];
+
+// Imprimira a toda la gente
+console.log(people);
+
+// 2. Remove "Dani"
+people.splice(people.indexOf("Dani"), 1);
+
+// 3. Remove "Juan"
+people.splice(people.indexOf("Juan"), 1);
+
+// 4. Mueve a "Luis" al frente
+let luisIndex = people.indexOf("Luis");
+let luis = people.splice(luisIndex, 1)[0];
+people.unshift(luis);
+
+// 5. Agrego mi Nombre
+people.push("Cecilia"); // Reemplaza "TuNombre" con tu nombre real
+
+// 6. Loop and break after "Maria"
+for (let person of people) {
+    console.log(person);
+    if (person === "Maria") break;
+}
+
+// 7. Index of "Maria"
+console.log("Index of Maria:", people.indexOf("Maria"));
+
+// Final array
+console.log("Final array:", people);
 
