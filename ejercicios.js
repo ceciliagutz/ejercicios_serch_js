f720c9d98ec1848185b6e4e5f50adb6f92639e1d
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-23 22:31:12 -0600

    feat: Se agrego el ejercicio 2

--- ejercicios.js
+++ ejercicios.js
@@ -16,3 +16,23 @@ function printOutString() {
 }
 printOutString(); // Imprime: "This is a sentence."
 
+
+//------------EJERCICIO 2-------------------
+
+
+/* 
+Exercise #2 (sugerencia map() )
+Write a function that:
+•	Takes in an array of numbers.
+•	Doubles the value of each number in the array.
+•	Prints out the new updated array.
+Example: Given an array [1, 2, 4, 5]. The output should be [2, 4, 8, 10]
+
+*/
+function elDobleNumeros(numbers) {
+  const elDoble = numbers.map(num => num * 2);
+  console.log(elDoble);
+}
+elDobleNumeros([1, 2, 4, 5]);
+
+
